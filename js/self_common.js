e631c52256d829c825934fc59421723377c3c3b2
TheoYao <TheoYao@163.com>
2018-03-25 23:21:02 +0800

    tmp again

--- js/self_common.js
+++ js/self_common.js
@@ -5,4 +5,12 @@ $(document).ready(function() {
         $.cookie('cookie_info', null);
         window.location.href = "index.html"
     });
+
+    if($.cookie('cookie_info')) {
+        var username = JSON.parse($.cookie('cookie_info')).username;
+        var identity = JSON.parse($.cookie('cookie_info')).identity;
+
+        $(".sign-area").empty();
+        $(".sign-area").html("<span id=\"sign-area-name\">"+username+"&nbsp;</span><a id=\"sign-out\" href=\"login.html#signin\">退出</a>");
+    }
 });
